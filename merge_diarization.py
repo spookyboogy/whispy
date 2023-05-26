374fbd9452d3a2564311870fc697b7cef3be6abc
spookyboogy <matt.tolstoy@gmail.com>
2023-05-25 20:04:08 -0500

    clarify filename handling parts of code (no effective change)

--- merge_diarization.py
+++ merge_diarization.py
@@ -371,7 +371,7 @@ def diarize_transcript(transcript_file, diarization_file, encoding='utf-8'):
         csvwriter.writerows(formatted_merger)
 
     # Write an additional csv with joined (condensed) dialogue lines
-    condensed_csv_out = os.path.splitext(transcript_file)[0] + "--merged.csv"
+    condensed_csv_out = os.path.splitext(csv_out)[0] + "--merged.csv"
     csv_f_out = condense_csv_lines(merger, f_name=condensed_csv_out, encoding=encoding)
 
     files_out = [txt_out, csv_out, csv_f_out]
